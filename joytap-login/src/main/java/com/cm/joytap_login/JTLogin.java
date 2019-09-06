8fa2d5ad4b9b03890206f564088e03a4a7d71922
wangyanWing <wangyan6@conew.com>
2019-09-06 20:55:05 +0800

    init

---
+++ joytap-login/src/main/java/com/cm/joytap_login/JTLogin.java
@@ -0,0 +1,10 @@
+package com.cm.joytap_login;
+
+import android.util.Log;
+
+public class JTLogin {
+
+    public static void login() {
+        Log.d("[JTLogin]", "login");
+    }
+}
---
+++ joytap-pay/src/main/java/com/cm/joytap_pay/JTPay.java
@@ -0,0 +1,10 @@
+package com.cm.joytap_pay;
+
+import android.util.Log;
+
+public class JTPay {
+
+    public static void pay() {
+        Log.d("[JTPay]", "pay");
+    }
+}
